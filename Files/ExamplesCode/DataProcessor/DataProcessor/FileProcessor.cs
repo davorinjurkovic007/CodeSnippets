7a00229fb067eda7c79e69acdd3a01a412a56d36
Davorin <davorin.jurkovic@gmail.com>
2025-12-08 23:36:39 +0100

    Workring with file part 1

--- Files/ExamplesCode/DataProcessor/DataProcessor/FileProcessor.cs
+++ Files/ExamplesCode/DataProcessor/DataProcessor/FileProcessor.cs
@@ -4,6 +4,10 @@ namespace DataProcessor;
 
 internal class FileProcessor
 {
+    private const string BackupDirectoryName = "backup";
+    private const string InProgressDirectoryName = "processing";
+    private const string CompleteDirectoryName = "complete";
+
     public string InputFilePath { get; }
 
     public FileProcessor(string filePath) => InputFilePath = filePath; 
@@ -11,5 +15,17 @@ internal class FileProcessor
     public void Process()
     {
         WriteLine($"Begin process of {InputFilePath}");
+        
+        // Check if file exists
+        if(!File.Exists( InputFilePath ))
+        {
+            WriteLine($"ERROR: file {InputFilePath} does not exist.");
+            return;
+        }
+
+        //string? rootDirectoryPath = new DirectoryInfo( InputFilePath ).Parent?.FullName;
+        string? rootDirectoryPath = new DirectoryInfo( InputFilePath ).Parent?.Parent?.FullName;
+
+        WriteLine($"Root data path is {rootDirectoryPath}");
     }
 }
