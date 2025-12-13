c4732a30b4fcdb3fbc11f0ce580de066aa968c22
Davorin <davorin.jurkovic@gmail.com>
2025-12-13 17:13:28 +0100

    Using Streams to Read and Write Text Context

--- Files/ExamplesCode/DataProcessor/DataProcessor/TextFileProcessor.cs
+++ Files/ExamplesCode/DataProcessor/DataProcessor/TextFileProcessor.cs
@@ -12,25 +12,48 @@ internal class TextFileProcessor
         OutputFilePath = outputFilePath;   
     }
 
+    //public void Process()
+    //{
+    //    // Using read all text
+    //    //string originalText = File.ReadAllText(InputFilePath);
+    //    //string processedText = originalText.ToUpperInvariant();
+    //    //File.WriteAllText(OutputFilePath, processedText);
+
+    //    // Using read all lines
+    //    try
+    //    {
+    //        string[] lines = File.ReadAllLines(InputFilePath);
+    //        lines[1] = lines[1].ToUpperInvariant(); // Assumes there is a line 2 in the file. 
+    //        File.WriteAllLines(OutputFilePath, lines);
+    //    }
+    //    catch(IOException ex)
+    //    {
+    //        // Log / retry
+    //        Console.WriteLine(ex);
+    //        throw;
+    //    }
+    //}
+
     public void Process()
     {
-        // Using read all text
-        //string originalText = File.ReadAllText(InputFilePath);
-        //string processedText = originalText.ToUpperInvariant();
-        //File.WriteAllText(OutputFilePath, processedText);
+        var openToReadFrom = new FileStreamOptions { Mode = FileMode.Open };
+        using var inputFileStream = new FileStream(InputFilePath, openToReadFrom);
+        using var inputStreamReader = new StreamReader(inputFileStream);
 
-        // Using read all lines
-        try
+        var createToWriteTo = new FileStreamOptions 
+        { 
+            Mode = FileMode.CreateNew,
+            Access = FileAccess.Write,
+        };
+        using var outputFileStream = new FileStream(OutputFilePath, createToWriteTo);
+        using var outputStreamWriter = new StreamWriter(outputFileStream);
+
+        while(!inputStreamReader.EndOfStream)
         {
-            string[] lines = File.ReadAllLines(InputFilePath);
-            lines[1] = lines[1].ToUpperInvariant(); // Assumes there is a line 2 in the file. 
-            File.WriteAllLines(OutputFilePath, lines);
+            string inputLine = inputStreamReader.ReadLine()!;
+            string processedLine = inputLine.ToUpperInvariant();
+            outputStreamWriter.WriteLine(processedLine);
         }
-        catch(IOException ex)
-        {
-            // Log / retry
-            Console.WriteLine(ex);
-            throw;
-        }
+
     }
 }
