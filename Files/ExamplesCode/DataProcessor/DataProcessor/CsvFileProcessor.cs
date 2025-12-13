967dbc5fb90441f902b7a21e98941f213c33f133
Davorin <davorin.jurkovic@gmail.com>
2025-12-13 21:35:19 +0100

    Configuring Trimming of Whitespace

--- Files/ExamplesCode/DataProcessor/DataProcessor/CsvFileProcessor.cs
+++ Files/ExamplesCode/DataProcessor/DataProcessor/CsvFileProcessor.cs
@@ -23,6 +23,7 @@ internal class CsvFileProcessor
         {
             Comment = '@',
             AllowComments = true,
+            TrimOptions = TrimOptions.Trim,
         };
         using CsvReader csvReader = new CsvReader(inputReader, csvConfiguration);
         IEnumerable<dynamic> records = csvReader.GetRecords<dynamic>();
