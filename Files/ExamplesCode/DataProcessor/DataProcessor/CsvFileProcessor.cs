6c925edc74adba18220ec15e789fe7306311ba44
Davorin <davorin.jurkovic@gmail.com>
2025-12-13 23:36:15 +0100

    Configuring CSV Blank Line Handlling

--- Files/ExamplesCode/DataProcessor/DataProcessor/CsvFileProcessor.cs
+++ Files/ExamplesCode/DataProcessor/DataProcessor/CsvFileProcessor.cs
@@ -24,6 +24,7 @@ internal class CsvFileProcessor
             Comment = '@',
             AllowComments = true,
             TrimOptions = TrimOptions.Trim,
+            IgnoreBlankLines = true, // this is default
         };
         using CsvReader csvReader = new CsvReader(inputReader, csvConfiguration);
         IEnumerable<dynamic> records = csvReader.GetRecords<dynamic>();
