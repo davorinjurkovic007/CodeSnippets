06cd80cafeed56f4207846d7d663e8fe24222e8c
Davorin <davorin.jurkovic@gmail.com>
2025-12-16 16:08:26 +0100

    Approval Tests 5 for .NET pluralsight -- Simple Demo begining

---
+++ Testing/Aproval Tests for .NET Pluralsight/SimpleDemo/SimpleDemo/NameJoiner.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleDemo
+{
+    public class NameJoiner
+    {
+        public string Join(string firstName, string surname)
+        {
+            return firstName + "-" + surname;
+        }
+    }
+}
