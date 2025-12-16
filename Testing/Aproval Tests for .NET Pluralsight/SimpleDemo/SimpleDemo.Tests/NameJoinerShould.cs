fe827d0428decc0f8182a9e75ed3feb40c8832eb
Davorin <davorin.jurkovic@gmail.com>
2025-12-16 17:05:16 +0100

    Finished SimpleDemo

---
+++ Testing/Aproval Tests for .NET Pluralsight/SimpleDemo/SimpleDemo.Tests/NameJoinerShould.cs
@@ -0,0 +1,19 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+
+namespace SimpleDemo.Tests;
+
+public class NameJoinerShould
+{
+    [Test]
+    [UseReporter(typeof(DiffReporter))]
+    public void JoinNames()
+    {
+        var sut = new NameJoiner();
+
+        string result = sut.Join("Jason", "Roberts");
+
+        // Use Approval Tests to verify ("assert") the result
+        Approvals.Verify(result);
+    }
+}
