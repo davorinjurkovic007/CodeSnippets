9e071b3cfa2d357b1361271c41d54301d1322a82
Davorin <davorin.jurkovic@gmail.com>
2025-12-18 22:33:44 +0100

    Testing Arrays, IEnumerables, and Dictionaries with Approval Tests

---
+++ Testing/Aproval Tests for .NET Pluralsight/StringUtilities/StringUtilities.Tests/WordReverserShould.cs
@@ -0,0 +1,32 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+
+namespace StringUtilities.Tests
+{
+    public class WordReverserShould
+    {
+        [Fact]
+        [UseReporter(typeof(DiffReporter))]
+        public void ReverseWords()
+        {
+            var names = new[] { "Sarah", "Gentry", "Amrit", "Peter", "Joan" };
+
+            var sut = new WordReverser();
+
+            var reversedNames = sut.Reverse(names).ToArray();
+
+            //Assert.Equal("haraS", reversedNames[0]);
+            //Assert.Equal("yrtneG", reversedNames[1]);
+            //Assert.Equal("tirmA", reversedNames[2]);
+            //Assert.Equal("reteP", reversedNames[3]);
+            //Assert.Equal("naoJ", reversedNames[4]);
+
+            Dictionary<string, string> inputAndOutput = names.Zip(reversedNames, 
+                (originalWord, reversedWord) => new {originalWord, reversedWord })
+                .ToDictionary(pair => pair.originalWord, pair => pair.reversedWord);
+
+            //Approvals.VerifyAll(reversedNames, "name", (x) => $"reversed '{x}'");
+            Approvals.VerifyAll(inputAndOutput);
+        }
+    }
+}
---
+++ Testing/Aproval Tests for .NET Pluralsight/StringUtilities/StringUtilities/WordReverser.cs
@@ -0,0 +1,22 @@
+﻿namespace StringUtilities
+{
+    public class WordReverser
+    {
+        public IEnumerable<string> Reverse(IEnumerable<string> strings)
+        {
+            foreach(var s in strings)
+            {
+                yield return ReverseWord(s);
+            }
+        }
+
+        private string ReverseWord(string s)
+        {
+            var chars = s.ToCharArray();
+
+            Array.Reverse(chars);
+
+            return new string(chars);
+        }
+    }
+}
