58d85afd6da523ca560aacd8dc06637523236447
Davorin <davorin.jurkovic@gmail.com>
2025-12-18 23:18:28 +0100

    Test Driven Development with Approval Tests

---
+++ Testing/Aproval Tests for .NET Pluralsight/TDD/HRReportGenerator/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace HRReportGenerator
+{
+    public class Employee
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int PayGrade { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ Testing/Aproval Tests for .NET Pluralsight/TDD/HRReportGenerator/EmployeeReportGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace HRReportGenerator
+{
+    public class EmployeeReportGenerator
+    {
+        public string Generate(Employee[] employees)
+        {
+            var reportText = new StringBuilder();
+
+            reportText.AppendLine("HR Employees Report");
+            reportText.AppendLine("-------------------");
+            reportText.AppendLine();
+
+            reportText.AppendLine("First Name               Last Name                  Pay Grade     DOB");
+            foreach (var employee in employees)
+            {
+                reportText.AppendLine($"{employee.FirstName,-25}{employee.LastName,-27}{employee.PayGrade,-14}{employee.DateOfBirth:MMMM d, yyyy}");
+            }
+
+            reportText.AppendLine();
+            reportText.Append($"Total Employees: {employees.Count()}");
+
+            return reportText.ToString();
+        }
+    }
+}
