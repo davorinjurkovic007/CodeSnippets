527aae0b4e12f5bad373f81e8ab1de08fe56e2d6
Davorin <davorin.jurkovic@gmail.com>
2025-12-20 18:47:30 +0100

    Fix Bitmap Report Builder Should

--- Testing/Aproval Tests for .NET Pluralsight/ImageDiffDemo/AnnualReportBuilder.Tests/BitmapReportBuilderShould.cs
+++ Testing/Aproval Tests for .NET Pluralsight/ImageDiffDemo/AnnualReportBuilder.Tests/BitmapReportBuilderShould.cs
@@ -19,7 +19,7 @@ namespace Tests
                 Title = "Annual Report",
                 ReportLines =
                 {
-                    "Line 1x",
+                    "Line 1",
                     "Line 2",
                     "Line 3",
                     "Line 4",
