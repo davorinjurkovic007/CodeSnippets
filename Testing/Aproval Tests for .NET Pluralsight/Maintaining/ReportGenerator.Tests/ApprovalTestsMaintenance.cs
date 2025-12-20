c157a7f5e3c9f6adef43b4eb046ed6f9e27981da
Davorin <davorin.jurkovic@gmail.com>
2025-12-20 20:47:11 +0100

    Maintaining Approved Files

---
+++ Testing/Aproval Tests for .NET Pluralsight/Maintaining/ReportGenerator.Tests/ApprovalTestsMaintenance.cs
@@ -0,0 +1,14 @@
+﻿using ApprovalTests.Maintenance;
+using Xunit;
+
+namespace ReportGenerator.Tests
+{
+    public class ApprovalTestsMaintenance
+    {
+        [Fact]
+        public void Maintenance()
+        {
+            ApprovalMaintenance.VerifyNoAbandonedFiles();
+        }
+    }
+}
---
+++ Testing/Aproval Tests for .NET Pluralsight/Maintaining/ReportGenerator.Tests/HtmlReportBuilderShould.cs
@@ -0,0 +1,34 @@
+﻿using ApprovalTests.Reporters;
+using Xunit;
+using ApprovalTests;
+using ApprovalTests.Maintenance;
+
+namespace ReportGenerator.Tests
+{
+    public class HtmlReportBuilderShould
+    {
+        [Fact]
+        [UseReporter(typeof(FileLauncherReporter),typeof(DiffReporter), typeof(ClipboardReporter))]
+        public void Build()
+        {
+            var model = new ReportModel
+            {
+                Title = "Annual Report",
+                ReportLines =
+                            {
+                                "Line 1",
+                                "Line 2",
+                                "Line 3",
+                                "Line 4",
+                                "Line 5"
+                            }
+            };
+
+            var sut = new HtmlReportBuilder(model);
+
+            string html = sut.Build();
+
+            Approvals.VerifyHtml(html);
+        }
+    }
+}
