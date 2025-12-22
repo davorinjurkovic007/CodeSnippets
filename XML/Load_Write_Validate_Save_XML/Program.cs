ffc16ab1b45140bc20043aa8c76d1fbe4e00dae8
Davorin <davorin.jurkovic@gmail.com>
2025-12-22 22:14:46 +0100

    Query and Aggregate XML Using XPath

--- XML/Load_Write_Validate_Save_XML/Program.cs
+++ XML/Load_Write_Validate_Save_XML/Program.cs
@@ -1,9 +1,16 @@
 ﻿using XMLSamples;
 
+// Process XML Files Easily Using .NET 6/7 ->Paul Sheriff 
+// https://www.codemag.com/Article/2312031/Process-XML-Files-Easily-Using-.NET-6-7
+
+
 // Create instance of view model
 //LoadingViewModel vm = new();
 //SaveViewModel vm = new();
-ValidateViewModel vm = new();
+//ValidateViewModel vm = new();
+//ElementViewModel vm = new();
+//AttributeViewModel vm = new();
+AggregateViewModel vm = new();
 
 // Call Sample Method
 //vm.LoadUsingXDocument();
@@ -17,7 +24,27 @@ ValidateViewModel vm = new();
 //vm.DataSetSave();
 
 //vm.ValidateXml();
-vm.ValidateXmlWithError();
+//vm.ValidateXmlWithError();
+
+//vm.GetAllXDocument();
+//vm.GetAllXElement();
+//vm.GetAllWithErrors();
+//vm.GetAllUsingExtensionMethod();
+//vm.GetASingleNode();
+//vm.GetACollectionOfNodes();
+//vm.GetLastNode();
+//vm.GetFirstThreeNodes();
+//vm.AddToClass();
+
+//vm.GetAll();
+//vm.GetASingleNode();
+//vm.GetACollectionOfNodes();
+
+//vm.Count();
+//vm.Sum();
+//vm.Average();
+//vm.Minimum();
+vm.Maximum();
 
 // Stop console to view results
 Console.ReadKey();
