a5e479b30fd9589f3ddd47a148ade80e32ab53c3
Davorin <davorin.jurkovic@gmail.com>
2025-12-31 00:58:14 +0100

    Async Serialization

---
+++ JSON/JSONSerialization/AsyncSerialization/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SerializationUsingJSONSerializer;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+// Create a new WeatherForecast object
+var weatherForecast = new WeatherForecast
+{
+    Date = DateTime.Parse("2021-12-01"),
+    TemperatureCelisius = 25,
+    Summary = "Hot"
+};
+
+// Name of the file where the JSON string is stored
+string fileName = "WeatherForecast.json";
+
+// Stream write data to a local file
+using FileStream createStream = File.Create(fileName);
+
+// Serialize it
+await JsonSerializer.SerializeAsync(createStream, weatherForecast);
+
+// Await and dispose the stream
+await createStream.DisposeAsync();
+Console.Clear();
